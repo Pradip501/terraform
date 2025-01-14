1971ef11f714dde86f4ed09f2b63d789dca0b274
Pradip501 <pradip.501jadhav@gmail.com>
2025-01-14 21:37:33 +0530

    update tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,3 @@
 provider "aws" {
   region     = "us-east-2"
-#   give access_key or secret_key if you creating from local system
-#   access_key = "my-access-key"
-#   secret_key = "my-secret-key"
 }
