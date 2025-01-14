5cdc745356102129b1a5ac13206afcd8de0b0f2d
Pradip501 <pradip.501jadhav@gmail.com>
2025-01-14 21:30:09 +0530

    update tf

---
+++ main.tf
@@ -0,0 +1,5 @@
+# key pair
+resource "aws_key_pair" "key" {
+  key_name   = "id_rsa"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF+Z2eFOblqA0yKfFxuKQrEVV7jeyGEdISCMNqoUP72t pradip@DESKTOP-G65PCJL"
+}
---
+++ provider.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+  region     = "us-east-2"
+#   give access_key or secret_key if you creating from local system
+#   access_key = "my-access-key"
+#   secret_key = "my-secret-key"
+}
