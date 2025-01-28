2bb86c6575c450fac6301a6c50beaced6ec485d4
Pradip Jadhav <78954620+Pradip501@users.noreply.github.com>
2025-01-28 08:27:45 +0530

    Delete main.tf

--- main.tf
+++
@@ -1,9 +0,0 @@
-# key pair
-provider "aws" {
-  region     = "us-east-2"
-}
-
-resource "aws_key_pair" "key" {
-  key_name   = "id_rsa"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF+Z2eFOblqA0yKfFxuKQrEVV7jeyGEdISCMNqoUP72t pradip@DESKTOP-G65PCJL"
-}
